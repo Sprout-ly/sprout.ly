09ccad5ab56b77cc5274238d27638e08bbc4f302
Stanford <stanfordhdai@gmail.com>
2020-02-19 11:19:10 -0800

    axios fetch working

--- src/display/components/MainPage.jsx
+++ src/display/components/MainPage.jsx
@@ -1,50 +1,49 @@
-import React, { useEffect, useState, useContext } from 'react';
-import { UserContext } from './context/UserContext.jsx';
+import React, { useEffect, useState, useContext } from "react";
+import { UserContext } from "./context/UserContext.jsx";
+import axios from "axios";
 
 function MainPage(props) {
-function handleClick() {
-  fetch('/test')
-  .then((response) => console.log(response))
-  .catch((err) => console.log(err));
+
+  function handleClick() {
+    fetch("/test")
+      .then(response => console.log(response))
+      .catch(err => console.log(err));
+  }
+
+  function fetchdata() {
+    const [searchText, setSearchText] = this.setState({});
+    const common_name = 'marijuana';
+    axios
+      .get(`https://cors-anywhere.herokuapp.com/https://trefle.io/api/plants?q=${common_name}&token=VGlCWVFZUU5mTkJLc1BYWUFYQ3F2Zz09`)
+      .then(data => console.log("data", data));
+  }
+
+  function onSignIn(googleUser) {
+    // Useful data for your client-side scripts:
+    var profile = googleUser.getBasicProfile();
+    console.log("ID: " + profile.getId()); // Don't send this directly to your server!
+    console.log("Full Name: " + profile.getName());
+    console.log("Given Name: " + profile.getGivenName());
+    console.log("Family Name: " + profile.getFamilyName());
+    console.log("Image URL: " + profile.getImageUrl());
+    console.log("Email: " + profile.getEmail());
+
+    // The ID token you need to pass to your backend:
+    var id_token = googleUser.getAuthResponse().id_token;
+    console.log("ID Token: " + id_token);
+  }
+
+  return (
+    <div>
+      <div class="g-signin2" data-onsuccess={onSignIn} data-theme="dark"></div>
+      <div>
+        <input type="text" className="searchInput" placeholder="Search..."></input>
+      </div>
+      <button type="button" onClick={fetchdata}>
+        CLICK ME TO FETCH
+      </button>
+    </div>
+  );
 }
-
-function fetchdata() {
-  fetch("https://trefle.io/api/plant",
-  {
-    method: 'GET',
-    mode: 'no-cors',
-    headers:{
-      "Authorization": 'Bearer VGlCWVFZUU5mTkJLc1BYWUFYQ3F2Zz09', 
-    }, 
-  })
-    .then(response => {
-      console.log(response)
-      response.json()})
-    .then(data => console.log(data));
-}
-
-function onSignIn(googleUser) {
-  // Useful data for your client-side scripts:
-  var profile = googleUser.getBasicProfile();
-  console.log("ID: " + profile.getId()); // Don't send this directly to your server!
-  console.log('Full Name: ' + profile.getName());
-  console.log('Given Name: ' + profile.getGivenName());
-  console.log('Family Name: ' + profile.getFamilyName());
-  console.log("Image URL: " + profile.getImageUrl());
-  console.log("Email: " + profile.getEmail());
-
-  // The ID token you need to pass to your backend:
-  var id_token = googleUser.getAuthResponse().id_token;
-  console.log("ID Token: " + id_token);
-}
-
-
-return (
-  <div>
-    <div class="g-signin2" data-onsuccess={onSignIn} data-theme="dark"></div>
-    <button type='button' onClick={handleClick}>CLICK ME!</button>
-    <button type='button' onClick={fetchdata}>CLICK ME TOOO</button>
-  </div>
-)};
 
 export default MainPage;
